3bcab6934b6655da18432a7d337ba54a85490832
fajfara <anze@thefajfar.com>
2018-03-23 18:36:46 +0100

    23.03.2018, 18:36
    Uploading what i learned today.
    Exploring Pandas library, reading data etc.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # exploring pandas python
+
+Just playing around, seeing what pandas library can do.
+
+Also using Jupyter notebook.
---
+++ pandaParty.py
@@ -0,0 +1,7 @@
+import pandas
+
+df1 = pandas.read_json("C:\\Users\\fajfa\\Desktop\\Projects\\Python\\Pandas\\exploring-pandas-python\\supermarkets.json")
+
+
+
+print(df1)
